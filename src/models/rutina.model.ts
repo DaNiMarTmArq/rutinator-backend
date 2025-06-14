520258146c4df3bec5022ec361cc5d8f1aa87279
mcnudi <mcnudi@hotmail.com>
2025-06-14 22:46:23 +0200

    subir ficheros para actualizar

--- src/models/rutina.model.ts
+++ src/models/rutina.model.ts
@@ -2,6 +2,7 @@ import { db } from "../db/db";
 
 export async function insertar(users_id:number, descripcion:string, nombre:string, defecto:boolean, shared:boolean,frequent:boolean): Promise<number> {
   const fechaActual = new Date().toISOString().slice(0, 19).replace('T', ' ');
-  const [result]:any = await db.query("INSERT INTO routines(users_id, description, name,is_default,created_at,is_shared,is_frequent )values (?,?,?,?,?,?,?)",[users_id,descripcion,nombre,defecto,fechaActual,shared,frequent]);
+  const defec = defecto ? 1 : 0;
+  const [result]:any = await db.query("INSERT INTO routines(users_id, description, name,is_default,created_at,is_shared,is_frequent )values (?,?,?,?,?,?,?)",[users_id,descripcion,nombre,defec,fechaActual,shared,frequent]);
   return result.insertId as number; 
 }
--- src/services/rutina.service.ts
+++ src/services/rutina.service.ts
@@ -5,7 +5,16 @@ import {
 
 
 //users_id:number, descripcion:string, nombre:string, defecto:boolean, shared:boolean,frequent:boolean
-export async function añadirRutina({users_id, descripcion, nombre, defecto,shared,frequent}:Rutina): Promise<number> {
-  const result= await insertar(users_id,descripcion,nombre,defecto,shared,frequent);
+export async function añadirRutina(rutina:any): Promise<number> {
+  const {
+    usuario,
+    descripcion,
+    name,
+    defecto,
+    shared = false,
+    frequent = false
+  } = rutina;
+  console.log("el valor de usuario es:",name);
+  const result= await insertar(usuario,descripcion,name,defecto,shared,frequent);
   return result;
 }
