1d1d40456caff4a9d1591d4ad5ed002a0f6a7470
mcnudi <mcnudi@hotmail.com>
2025-06-19 23:14:21 +0200

    subida alta y modificacion

--- src/routes/rutina.routes.ts
+++ src/routes/rutina.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getRutinasByUser, addRutina } from "../controllers/rutina.controller";
+import { getRutinasByUser, addRutina,modRutina,getRutinasId } from "../controllers/rutina.controller";
 
 const router = Router();
 
-// router.post("/", (req, res) => addRutina(req, res));
 router.post("/", addRutina);
-router.get('/user/:userId', getRutinasByUser); 
+router.get('/user/:userId', getRutinasByUser);
+router.get('/:id', getRutinasId);
+router.put("/", modRutina);
 
 export default router;
