dc6ddbe25635a274542dd4efbdb29f0e30e52bd7
Daniel Martinez <danimartinezmarquez@gmail.com>
2025-06-22 18:13:07 +0200

    Arreglado los goals

--- src/services/goals.service.ts
+++ src/services/goals.service.ts
@@ -7,12 +7,8 @@ import {
   getGoalsByName,
   removeGoalsFromUser,
 } from "../models/goals.model";
-import {
-  Goals,
-  GoalsDetails,
-} from "../models/interfaces/goals.interfaces";
+import { Goals, GoalsDetails } from "../models/interfaces/goals.interfaces";
 import { findById } from "../models/user.model";
-
 
 export async function getByUserId(userId: string): Promise<Goals[]> {
   const user = await findById(parseInt(userId));
@@ -22,42 +18,37 @@ export async function getByUserId(userId: string): Promise<Goals[]> {
   return await getAllGoalsByUser(userId);
 }
 
+export async function addGoal(goalsDetails: GoalsDetails): Promise<Goals> {
+  const { userId, interestId, goalsName, goalsDescription, hoursPerWeek } =
+    goalsDetails;
+  console.log(goalsDetails);
+  const newGoalId = await addGoalsToUser(
+    userId,
+    interestId,
+    goalsName,
+    goalsDescription,
+    hoursPerWeek
+  );
+  console.log(newGoalId);
 
-export async function addGoal(
-  goalsDetails: GoalsDetails
-): Promise<Goals> {
-  const { userId, interestId, goalsName, goalsDescription, hoursPerWeek } = goalsDetails;
-  console.log(goalsDetails)
-  const newGoalId =  await addGoalsToUser(userId, interestId, goalsName, goalsDescription, hoursPerWeek );
-  console.log(newGoalId)
-
-  const goal = { 
+  const goal = {
     id: newGoalId,
     users_id: userId,
     interests_id: interestId,
     goals_name: goalsName,
-    goals_description: goalsDescription,
-    hours_per_week: hoursPerWeek
+    description: goalsDescription,
+    hours_per_week: hoursPerWeek,
   };
 
   return goal;
 }
 
-
-
-export async function deleteGoalsFromUser(
-  userId: string,
-  goalsName: string
-) {
+export async function deleteGoalsFromUser(userId: string, goalsName: string) {
   const goals = await getGoalsByName(goalsName);
   if (!goals) throw new GoalsNotFoundError();
   await removeGoalsFromUser(parseInt(userId), goals.id);
 }
 
-
-export async function deleteGoalsById(
-  userId: string,
-  goalsId: string
-) {
+export async function deleteGoalsById(userId: string, goalsId: string) {
   await removeGoalsFromUser(parseInt(userId), parseInt(goalsId));
 }
