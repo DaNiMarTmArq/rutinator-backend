083191c42b7d430ded545850a480a5ec038ffe84
carlossr15 <csobrado@ucm.es>
2025-06-28 20:45:57 +0200

    cambio en query

--- src/models/activity.model.ts
+++ src/models/activity.model.ts
@@ -33,16 +33,13 @@ export async function findActivityByRoutineByDefault(id: number): Promise<Activi
       FROM activities a
       JOIN routines_versions rv ON a.routines_versions_id = rv.id
       JOIN routines r ON r.id = rv.routines_id
-      JOIN users u ON u.id = r.users_id
-      WHERE u.id = ?
-      AND r.is_default = 1
       AND rv.id = (
         SELECT MAX(rv2.id)
         FROM routines_versions rv2
         JOIN routines r2 ON rv2.routines_id = r2.id
-        WHERE r2.users_id = ? AND r2.is_default = 1
+        WHERE r2.users_id = ? AND rv2.is_selected = 1
   );`,
-    [id, id] 
+    [id] 
   );
   return rows as Activity[];
 }
