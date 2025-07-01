db7d2693183987514e5a87edab9d9cc1d001ca6c
Pepe Puertas Alonso <pepepuertasalonso4@gmail.com>
2025-07-01 20:33:08 +0200

    Creando lógica para recordar la contraseña

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -15,6 +15,8 @@ import {
   updateImage,
   updateUser,
   deleteUser,
+  findEmailByName,
+  updatePassword,
 } from "../models/user.model";
 import {
   AppError,
@@ -23,6 +25,7 @@ import {
   UserAlreadyExistsError,
 } from "../errors/errors";
 import { HttpStatus } from "../errors/http.errors";
+import nodemailer from "nodemailer";
 
 export async function login(
   credentials: UserLoginRequest
@@ -199,3 +202,80 @@ export async function deleteUserByUsername(username: string) {
 
   const result = await deleteUser(username);  
 }
+
+export async function getEmailByUsername(username: string): Promise<any> {
+  
+  const email = await findEmailByName(username);
+  if (!email) {
+    throw new InvalidUserCredentials(); 
+  }
+
+  console.log("User email found:", email);
+  return email;
+}
+
+export async function sendVerificationCode(
+  emailDestino: string,
+  codigo: string
+): Promise<void> {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "rutinatorunir@gmail.com",
+      pass: "cwsj ztqk vwuc izkf", 
+    },
+  });
+
+  const mailOptions = {
+    from: '"Rutinator" <rutinatorunir@gmail.com>',
+    to: emailDestino,
+    subject: "Código de verificación",
+    html: `
+      <div style="font-family: Arial, sans-serif; padding: 20px; background-color: #f2f2f2;">
+        <div style="max-width: 600px; margin: auto; background: white; padding: 30px; border-radius: 10px; box-shadow: 0 2px 10px rgba(0,0,0,0.1);">
+          <h2 style="color: #0fa3b1; text-align: center;">Verificación de cuenta</h2>
+          <p>Hola,</p>
+          <p>Has solicitado un código para recuperar tu cuenta en <strong>Rutinator</strong>. Introduce el siguiente código en la aplicación:</p>
+  
+          <div style="margin: 30px auto; padding: 20px; background-color: #f9fafb; border-radius: 8px; text-align: center; border: 1px solid #ddd; width: fit-content;">
+            <span style="font-size: 24px; font-weight: bold; letter-spacing: 2px; color: #333;">${codigo}</span>
+          </div>
+  
+          <p style="font-size: 14px; color: #555;">
+            Si no has solicitado este código, puedes ignorar este correo.
+          </p>
+  
+          <p style="font-size: 14px; color: #555;">Gracias,<br/>El equipo de Rutinator</p>
+        </div>
+      </div>
+    `,
+  };
+  
+
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error enviando correo:", error);
+        reject(error);
+      } else {
+        console.log("Correo enviado:", info.response);
+        resolve();
+      }
+    });
+  });
+}
+
+export async function updatePasswordByUsername(
+  username: string,
+  newPassword: string
+): Promise<void> {
+  const user = await findByName(username);
+
+  if (!user) {
+    throw new Error("Usuario no encontrado");
+  }
+  const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+  const result = await updatePassword(username, hashedPassword);
+}
+
